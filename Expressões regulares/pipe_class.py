49d55414203e5cd5f733ac29eae0e84208a835ef
Beatriz Costa <120328207+xbeatriz@users.noreply.github.com>
2025-07-28 12:29:15 +0100

    pipe

--- Expressões regulares/pipe_class.py
+++ Expressões regulares/pipe_class.py
@@ -1,1 +1,29 @@
 # | => A|B ou B uma coisa ou outra
+import re 
+
+def encontrar(regex, texto, opcao = 'match'):
+    """opcao ==> match, search ou findall"""
+    p = re.compile(regex)
+    m = p.__getattribute__(opcao)(texto)
+    if m: 
+        if opcao != 'findall':
+            return print(m.group())
+        return print(m)
+    
+
+regex = r'Ta\B|ta\B' # final da palavra. se fosse inicio da palavra \bta
+texto = 'Tampa a panela Roberta Batata'
+# encontrar(regex, texto, 'findall')
+p = re.compile(regex)
+matches = p.finditer(texto)
+for m in matches:
+    print(m)
+
+regex = r'eu@(gmail|hotmail).com'
+texto = 'eu@gmail.com e eu@hotmail.com'
+# encontrar(regex, texto, 'findall')
+p = re.compile(regex)
+matches = p.finditer(texto)
+for m in matches:
+    print(m.group(0))
+    print(m.group(1))
