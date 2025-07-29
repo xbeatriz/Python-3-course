e78ee7067d9514a80f97d96442ea0cfe1875d3f6
Beatriz Costa <120328207+xbeatriz@users.noreply.github.com>
2025-07-29 12:36:18 +0100

    contar palavras

---
+++ Counting words/counter_class.py
@@ -0,0 +1,12 @@
+import collections
+
+lista_de_palavras = 'algumas palavras a serem contadas\
+                    vou repetir algumas palavras, porque\
+                    quero ter bastantes palavras'
+
+counter_freq = collections.Counter(lista_de_palavras)
+print(counter_freq)
+
+
+# o nosso contador de palavras
+
